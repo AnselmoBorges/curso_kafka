bb9f180a200a9f4a83131bb353bd24643af97fa5
Anselmo Borges <anselmoborges@gmail.com>
2020-01-09 19:28:30 -0300

    Adicionando Consumer

--- kafka-beginners-course/src/main/java/com/github/anselmoborges/kafka/tutorial1/ConsumerDemo.java
+++ kafka-beginners-course/src/main/java/com/github/anselmoborges/kafka/tutorial1/ConsumerDemo.java
@@ -1,4 +1,47 @@
 package com.github.anselmoborges.kafka.tutorial1;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 public class ConsumerDemo {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
+
+        String bootstrapServer = "localhost:9092";
+        String groupID = "app_teste";
+        String topic = "primeiro_topico";
+
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        // Criando o consumer
+        KafkaConsumer<String,String> consumer = new KafkaConsumer<String, String>(properties);
+
+        // Mandando o consumer para o tópico:
+        consumer.subscribe(Collections.singleton(topic));
+
+        // veririficar se tem novos dados:
+        while(true){
+            ConsumerRecords<String,String> records =
+                    consumer.poll(Duration.ofMillis(100)); // Dizem que é novo no kafka 2.0.0
+
+            for (ConsumerRecord<String,String> record: records) {
+                logger.info("Key: " + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+    }
 }
